cce3982bc9e9d1d05f7c244c9b020017a6f6a2aa
Cainã Perotti Barbosa <caiperotti@gmail.com>
2023-02-05 14:14:11 -0300

    server fix

--- src/controller/app.ts
+++ src/controller/app.ts
@@ -6,6 +6,6 @@ export const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003, () => {
+app.listen(process.env.PORT || 3003, () => {
     console.log("Server running on port 3003")
 })
